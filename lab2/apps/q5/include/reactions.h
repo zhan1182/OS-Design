c97e0dbaea6b49d18d8e307a967e43fe92f9dda5
Jinyi Zhang <zhan1182@purdue.edu>
2016-02-11 20:37:21 -0500

    q5 v1

--- lab2/apps/q5/include/reactions.h
+++ lab2/apps/q5/include/reactions.h
@@ -4,6 +4,20 @@
 #ifndef __USERPROG__
 #define __USERPROG__
 
+
+typedef struct molecules
+{
+  int init_h2o;
+  int init_so4;
+  sem_t h2o;
+  sem_t h2;
+  sem_t o2;
+  sem_t so4;
+  sem_t so2;
+  sem_t h2so4;
+} Molecules;
+
+
 #define NUMBER_OF_PROCESS 5
 
 #define REACTION_1_TO_RUN "reaction_1.dlx.obj"
--- lab2/apps/q5/injection_so4/injection_so4.c
+++ lab2/apps/q5/injection_so4/injection_so4.c
@@ -9,6 +9,9 @@ void main(int argc, char ** argv)
   uint32 h_mem;
   sem_t s_procs_completed;
 
+  Molecules * mols;
+  int ct;
+
   if (argc != 3) { 
     Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore>\n"); 
     Exit();
@@ -19,10 +22,19 @@ void main(int argc, char ** argv)
   s_procs_completed = dstrtol(argv[2], NULL, 10);
 
   // Map shared memory page into this process's memory space
-
+  if ((mols = (Molecules *) shmat(h_mem)) == NULL) {
+    Printf("Could not map the shared page to virtual address in "); Printf(argv[0]); Printf(", exiting..\n");
+    Exit();
+  }
+  
+  // inject h2o
+  for(ct = 0; ct < mols->init_so4; ct++){
+    sem_signal(mols->so4);
+    Printf("A molecule SO4 is injected\n");
+  }
 
   // Signal the semaphore to tell the original process that we're done
-  Printf("Consumer: PID %d is complete.\n", getpid());
+  Printf("injection_so4: PID %d is complete.\n", getpid());
 
 
   if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
