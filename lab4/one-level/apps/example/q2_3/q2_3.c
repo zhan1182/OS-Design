a6f914b86e4fec76e5da0fcb96172c4dbfbfb327
Jinyi Zhang <zhan1182@purdue.edu>
2016-03-30 22:00:06 -0400

    add q2.3

---
+++ lab4/one-level/apps/example/q2_3/q2_3.c
@@ -0,0 +1,33 @@
+#include "usertraps.h"
+#include "misc.h"
+
+void main (int argc, char *argv[])
+{
+  sem_t s_procs_completed; // Semaphore to signal the original process that we're done
+
+  int * ptr = NULL;
+
+  if (argc != 2) { 
+    Printf("Usage: %s <handle_to_procs_completed_semaphore>\n"); 
+    Exit();
+  } 
+
+  // Convert the command-line strings into integers for use as handles
+  s_procs_completed = dstrtol(argv[1], NULL, 10);
+
+  
+  // Signal the semaphore to tell the original process that we're done
+  if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("hello_world (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
+    Exit();
+  }
+
+
+  ptr = 0x4001;
+
+  Printf("about to access memory outside of page: (%d)\n", getpid());
+
+  *ptr = 5;
+
+  Printf("q2.2 access memory outside of currently allocated pages (%d): Done!\n", getpid());
+}
