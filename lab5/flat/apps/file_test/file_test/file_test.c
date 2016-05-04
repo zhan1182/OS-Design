8d4361b28e002b7eb8847065363ec7db1674b28c
Jinyi Zhang <zhan1182@purdue.edu>
2016-05-03 23:05:28 -0400

    q6 init

---
+++ lab5/flat/apps/file_test/file_test/file_test.c
@@ -0,0 +1,6 @@
+#include "usertraps.h"
+
+void main (int argc, char *argv[])
+{
+  run_os_tests();
+}
---
+++ lab5/flat/apps/file_test/include/file_test.h
@@ -0,0 +1,8 @@
+#ifndef __FILE_TEST_H__
+#define __FILE_TEST_H__
+
+#ifndef NULL
+#define NULL (void *)0x0
+#endif
+
+#endif
